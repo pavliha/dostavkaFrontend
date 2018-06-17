bcd282162698e4bfb32ef45291199243b87bb5cd
Oleg Tyunin <kuksik007@gmail.com>
2018-06-17 21:32:24 +0300

    add Marketing

--- src/components/@index/IndexScene.jsx
+++ src/components/@index/IndexScene.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Banner from './Banner'
+import Marketing from './Marketing'
 
 const IndexScene = () =>
   <div>
     <Banner />
+    <Marketing />
   </div>
 
 export default IndexScene
