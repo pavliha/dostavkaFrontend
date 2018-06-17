0019b9aabc70f14fa7fd850c92120a1c3764637c
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2018-06-17 13:33:24 +0300

    add create-form layout

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import promiseMiddleware from 'redux-promise-middleware'
 
 const store = createStore(
-  {},
+  combineReducers({}),
   composeWithDevTools(applyMiddleware(
     thunk,
     promiseMiddleware(),
