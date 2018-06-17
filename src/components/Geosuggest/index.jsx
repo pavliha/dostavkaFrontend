7c935e9c6b5f908081b53e272a1def39c339bb8d
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2018-06-17 21:30:24 +0300

    fallback to plain text on react-geosuggest

--- src/components/Geosuggest/index.jsx
+++ src/components/Geosuggest/index.jsx
@@ -12,32 +12,11 @@ class Geosuggest extends Component {
   }
 
   componentDidMount() {
-    if (typeof window.google !== 'undefined' && typeof window.google.maps !== 'undefined') {
-      const inputElement = document.getElementsByName(this.props.name)[0]
-      this.apiObj = new window.google.maps.places.Autocomplete(inputElement, this.props.options)
-      this.apiObj.addListener('place_changed', () => {
-        this.props.onChange(this.props.name, this.apiObj.getPlace())
-        this.setState({
-          value: this.apiObj.getPlace().formatted_address,
-        })
-      })
-    } else {
-      console.warn('Google API object is not defined')
-      this.props.onChange(this.props.name, {
-        formatted_address: this.state.value,
-        geometry: {
-          location: {
-            lat: () => 0,
-            lng: () => 0,
-          },
-        },
-      })
-
-    }
   }
 
   handleChange = e => {
     this.setState({ value: e.target.value })
+    this.props.onChange(this.props.name, e.target.value)
   }
 
   handleBlur = () => {
