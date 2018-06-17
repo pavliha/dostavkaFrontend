5d598e1ca3f2d38841067fb05f63c1329dcac967
LexaLukivka <lukerlexa@gmail.com>
2018-06-17 18:53:47 +0300

    add Login

--- src/components/LayoutScene.jsx
+++ src/components/LayoutScene.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom'
 import CargoCreateScene from './@cargo-create/CargoCreateScene'
 import RegisterScene from './@register/RegisterScene'
 import withTheme from '../utils/withTheme'
+import LoginScene from './@login/LoginScene'
 
 const LayoutScene = () =>
   <main>
     <Switch>
       <Route path="/cargo" component={CargoCreateScene} />
       <Route path="/register" component={RegisterScene} />
+      <Route path="/login" component={LoginScene} />
     </Switch>
   </main>
 
